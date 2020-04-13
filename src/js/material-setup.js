22ef97e52ae9aba0875916900e3898174363e8ef
Aeol <carlj15.cj@gmail.com>
2020-04-13 15:00:51 +0800

    theme changer component

--- src/js/material-setup.js
+++ src/js/material-setup.js
@@ -1,25 +1,14 @@
 import { MDCRipple           } from '@material/ripple'
 import { MDCTopAppBar        } from '@material/top-app-bar'
-import { MDCIconButtonToggle } from '@material/icon-button'
-import { MDCMenu             } from '@material/menu'
-import { MDCList             } from '@material/list'
-import { MDCTextField        } from '@material/textfield'
-import { get_country         } from './covid-data.js'
-import { get_country_list    } from './covid-data.js'
 import { el                  } from './utils'
 import { style_remove        } from './utils'
 import { style_apply         } from './utils'
-import { remove_child        } from './utils'
 import { get_config          } from './covid-config.js'
-import { update_config       } from './covid-config.js'
-import { add_to_countries    } from './covid-config.js'
 import * as search_bar from './mdc-components/search-bar'
 import * as card_menu from './mdc-components/card-menu'
 import * as snackbar from './mdc-components/snackbar'
-import Clusterize              from 'clusterize.js'
-import Fuse                    from 'fuse.js'
+import * as theme_changer from './mdc-components/theme-changer'
 import add_country             from './add-country.js'
-import change_theme            from './theme-changer.js'
 
 async function setup_app_bar() {
     const app_bar = el('top-app-bar')
@@ -45,20 +34,6 @@ async function setup_app_bar() {
     }
 }
 
-async function setup_theme_changer(config_theme) {
-    const change_btn   = el('theme-change')
-    const theme_change = new MDCIconButtonToggle(change_btn)
-
-    change_theme(config_theme)
-    theme_change.on = config_theme === 'dark'
-
-    theme_change.listen('MDCIconButtonToggle:change', ({ detail: { isOn }}) => {
-        const new_theme = isOn ? 'dark' : 'light'
-        change_theme(new_theme)
-        update_config({ theme: new_theme })
-    })
-}
-
 function setup_ripples() {
     [].map.call(document.querySelectorAll('.ripple'), (e) => {
         const ripple = new MDCRipple(e)
@@ -80,7 +55,7 @@ function setup_cards(config) {
 export default async function material_setup() {
     const config = await get_config()
 
-    setup_theme_changer(config.theme)
+    theme_changer.initialize(config)
     setup_app_bar()
     search_bar.initialize(config)
     card_menu.initialize()
